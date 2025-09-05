687cac395e494a750e426846d46a07765ed5f3b5
yogendra-97 <yogendranarayanp7799@gmail.com>
2025-09-06 01:03:22 +0530

    modified:   test_flipkart.py on 6/Sep

--- test_flipkart.py
+++ test_flipkart.py
@@ -44,7 +44,7 @@ def test_flipkart():
         print("\nJust checking exception Handling for " + offer.text)
 
     except:
-        print("We could not find any offers for you, please try later..!!")
+        print("Currently, No offer are available for you, checkout later..!!")
 
 def test_cont():
     try:
